857bcd9ed68b1a708e3136f292efc534ca5e4ee9
Lan Leaf <1591747875@qq.com>
2016-03-29 19:20:47 +0800

    Update Test.java

--- compounding_4.0-s-Test/Test.java
+++ compounding_4.0-s-Test/Test.java
@@ -10,7 +10,12 @@ public class Test {
 	@Before
 	public void setUp() throws Exception {
 	}
-	
+	@org.junit.Test
+	 public void testinit() {
+        	fuli_4 fuli = new fuli_4();
+	 	double f = fuli.init(100000,10,3);
+		 Assert.assertEquals(f, 134390, 3.0);
+	 }
 	//测试当输入值为空时，计算结果返回0.0
 	@org.junit.Test
 	public void testReturn() {
